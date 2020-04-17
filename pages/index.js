6f11563a3af63db8bbfb36e9a64c60e4f4cd4bad
moumniheithem <moumniheithem@gmail.com>
2020-04-17 17:58:32 +0100

    work on styling

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from "react";
 import axios from "axios";
 import Link from "next/link";
-import wp from '../wp-config';
+import wp from "../wp-config";
 
 export default class extends Component {
   state = () => ({
@@ -11,10 +11,7 @@ export default class extends Component {
 
   // Resolve promise and set initial props.
   static async getInitialProps() {
-    // Make request for posts.
     const response = await axios.get(wp.allPosts);
-
-    // Return response to posts object in props.
     return {
       posts: response.data,
     };
@@ -23,17 +20,72 @@ export default class extends Component {
   render() {
     return (
       <Fragment>
-        <div className="post">
-          {this.props.posts.map((post) => {
-            return (
-              <div className="post" key={post.id}>
-                <h3>{post.title.rendered}</h3>
-                <small>{post.date}</small>
-                <div dangerouslySetInnerHTML={{__html: post.excerpt.rendered}}/>
-                <a href={post.slug} className="readmore">read more</a>
-              </div>
-            );
-          })}
+        <style jsx>{`
+          nav {
+            background-color: #000;
+            color: #fff;
+            padding: 1em 100px;
+          }
+          nav li {
+            display:inline-block;
+            margin-right:30px;
+          }
+          nav a {
+            text-decoration: none;
+            color: white;
+          }
+          .masthead {
+            border: solid 1px red;
+            width: 100%;
+              height: 90vh;
+              overflow: hidden;
+              text-align: center;
+              display: -webkit-box;
+              display: flex;
+              -webkit-box-pack: center;
+              justify-content: center;
+              -webkit-box-align: center;
+              align-items: center;
+              padding: 7vw;
+              background: radial-gradient(ellipse at center, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0) 37%, rgba(0, 0, 0, 0.65) 100%), url(/mountains-masthead.jpg) no-repeat center center scroll;
+              background-size: cover;
+              color: #333;
+          }
+          }
+        `}</style>
+
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <a href="/" class="">
+                  BlogName
+                </a>
+              </li>
+              <li>
+                <a href="/about">About</a>
+              </li>
+            </ul>
+          </nav>
+          <section class="masthead">
+            <h1>Headless WordPress on the JAMstack</h1>
+          </section>
+          <div className="post">
+            {this.props.posts.map((post) => {
+              return (
+                <div className="post" key={post.id}>
+                  <h3>{post.title.rendered}</h3>
+                  <small>{post.date}</small>
+                  <div
+                    dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+                  />
+                  <a href={post.slug} className="readmore">
+                    read more
+                  </a>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </Fragment>
     );
--- wp-config.js
+++ wp-config.js
@@ -1,7 +1,8 @@
-const baseUrl = 'https://css-tricks.com/wp-json';
+const baseURL = 'https://css-tricks.com/wp-json';
 
 const wp = {    
-    allPosts: `${baseUrl}/wp/v2/posts?page=1&per_page=20&_embed=1`,     
+    allPosts: `${baseURL}/wp/v2/posts?page=1&per_page=20&_embed=1`,
+    getTags: `${baseURL}`  
 }
 export default wp
 
